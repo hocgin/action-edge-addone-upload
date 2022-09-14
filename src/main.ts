fe9519c3b4b88c5db183dd9c7ebb0223adb921c5
hocgin <hocgin@gmail.com>
2022-09-14 20:22:07 +0800

    feature

--- src/main.ts
+++ src/main.ts
@@ -37,7 +37,7 @@ try {
     core.setFailed(error?.message);
 }
 
-export let debugPrintf = (...args: any) => {
+export function debugPrintf(...args: any) {
     if (getInput().debug) {
         console.log(...args);
     }
