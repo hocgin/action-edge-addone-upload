1e6f57b36660423902006fa9c0700b0c659a3647
hocgin <hocgin@gmail.com>
2022-09-15 11:36:00 +0800

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import {run} from "./core";
+import * as core from "@actions/core";
+
+export interface Inputs {
+    debug?: boolean;
+}
+
+export interface Outputs {
+    // ..
+    [key: string]: any
+}
+
+let getInput = (): Inputs => ({
+    debug: core.getInput('debug') === 'true'
+})
+
+let handleOutput = (output: Outputs = {}) => {
+    Object.keys(output).forEach((key) => core.setOutput(key, output[key]));
+    debugPrintf('输出变量: ', output);
+};
+
+try {
+    handleOutput(run(getInput()))
+} catch (error: any) {
+    core.setFailed(error?.message);
+}
+
+export let debugPrintf = (...args: any) => {
+    if (getInput().debug) {
+        console.log(...args);
+    }
+}
